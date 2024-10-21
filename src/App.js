62267e9303f7a5b3b21ade9493b6f5126229a829
Aakriti Banjara <banjarabinda@gmail.com>
2024-10-21 18:08:45 +0545

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Profile from './PortfolioContainer/Home/Profile';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,64 @@
+﻿import React from 'react';
+import Typical from 'react-typical'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+      <div className='profile-parent'> 
+        
+          <div className='colz'>
+            {/* yesko baki xa hai link haru milauna */}
+            <a href='https://www.facebook.com'> 
+              <i className='fa fa-facebook-square'></i> 
+            </a>
+            <a href='https://plus.google.com'>
+              <i className='fa fa-google-plus-square'></i>
+            </a>
+            <a href='https://www.instagram.com'>
+              <i className='fa fa-instagram'></i>
+            </a>
+            <a href='https://www.youtube.com'>
+              <i className='fa fa-youtube-square'></i>
+            </a>
+            <a href='https://www.twitter.com'>
+              <i className='fa fa-twitter'></i>
+            </a>
+          </div>
+          <div className='profile-details'>
+            <span className='Primary-text'>
+              {" "}
+              Hello, I'M <span className='highlighted-text'>Aakriti Banjara</span>
+            </span>
+        </div>
+        <div className='profile-details-role'>
+          <span className='primary-text'>
+            {" "}
+            <h1>
+            {" "}
+            <Typical 
+            loop = {Infinity}
+            steps={[
+                  "Ethusiastic Dev 🦾",
+                  1000,
+                  "Full Stack Developer 💻",
+                  1000,
+                  "Machine learning 🔗",
+                  1000,
+                  "AI ⚒🔧 ",
+                  1000,
+                  "Game Development 🎮🕹🎲 ",
+                  1000,
+                  "Deep Learning 👇📖",
+                  1000,
+                  "Neural Network 🧬📎 ",
+                  1000,
+
+            ]}
+            />
+            </h1>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
